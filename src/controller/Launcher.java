1376df14eb3642f5ad522f0b732e6c71cf3ccf7d
Remi DE WISPELAERE <rdewispelaere@excilys?com>
2019-03-14 16:50:49 +0100

    Finalisation de la pagination ( + renommage Page -> PageManager)

--- src/controller/Launcher.java
+++ src/controller/Launcher.java
@@ -15,6 +15,7 @@ import org.apache.commons.cli.Options;
 import dao.DAOFactory;
 import model.Company;
 import model.Computer;
+import model.PageManager;
 import dao.CompanyDaoImpl;
 import dao.ComputerDaoImpl;
 import controller.Cli;
@@ -24,38 +25,38 @@ public class Launcher {
 	static DAOFactory daoFactory = DAOFactory.getInstance();
 	static ComputerDaoImpl computerDao = (ComputerDaoImpl) daoFactory.getComputerDao();
 	static CompanyDaoImpl companyDao = (CompanyDaoImpl) daoFactory.getCompanyDao();
-	
+
 	static Scanner scanner = new Scanner(System.in);
-	
+
 	public Launcher() {
-		
+
 	}
-	
+
 	public static void main(String[] args) throws org.apache.commons.cli.ParseException{ 
-		
+
 		System.out.println("-_-_-_-_-_INIT-_-_-_-_-");
-		 
+
 		//////////////////////////////////////////////////////////////
 		/*CLI par ligne de commande
-		  * A retirer une fois l'interface d'utilisation finie
-		  */
+		 * A retirer une fois l'interface d'utilisation finie
+		 */
 		////////CLI////////
 		/*final Options options = Cli.configParameters();
 		final CommandLineParser parser = new DefaultParser();
 		final CommandLine line = parser.parse(options, args);
-		
+
 		//Table name option
 		String tableName = line.getOptionValue("table", "computer");
-		
+
 		//Query type option
 		char queryType = line.getOptionValue("querytype", "s").charAt(0);
-		
+
 		//Name argument for the query
 		String nameQuery = "";
 		if(line.hasOption("nameArg")) {
 			nameQuery = line.getOptionValue("nameArg");
 		}
-		
+
 		//Limit option
 		int limitNumber;
 		if(line.hasOption("limit")) {
@@ -66,15 +67,15 @@ public class Launcher {
 			}
 		}*/
 		///////////////////////////////////////////////////////////////////////////////////
-		
+
 		int choice;
-		
+
 		System.out.println("-_-_-_-_-_START-_-_-_-_-");
-		
+
 		do {
 			printMenu();
 			choice = askChoice(6);
-			
+
 			switch(choice) {
 			case 1: //List of computers
 				printAllComputer();
@@ -97,81 +98,81 @@ public class Launcher {
 			default:
 				//	
 			}
-			
+
 			//PAUSE//
 			if(choice != 0) 
 				pause();
-			
+
 		}while(choice != 0);
-		
-		
+
+
 		System.out.println("-_-_-_-_-_STOP-_-_-_-_-");
 
 	}
-	
+
 	public static void createNewComputer() {
 		//Name
 		System.out.print("ENTER THE NAME : ");
 		String nName = scanner.nextLine();
-		
+
 		//Company id
 		int nCompany = askCompanyId();
-		
+
 		//Introduced date
 		Date nIntroDate = askIntroducedDate();
 		//Discontinued date
 		Date nDisconDate = askDiscontinuedDate();
-		
+
 		computerDao.add(new Computer(0, nName, nCompany, nIntroDate, nDisconDate)); //Id est auto incrementee par le dao
 	}
-	
+
 	public static void updateComputer() {
 		System.out.println("Search which computer you want to update\n");
 		Computer cpuToUpdate = searchComputerDetails();
-		
+
 		//Name
 		System.out.print("ENTER THE NEW NAME (EMPTY TO KEEP THE SAME [" + cpuToUpdate.getName() + "]) : " );
 		String nName = scanner.nextLine();
-		
+
 		if(nName.isEmpty())
 			nName = cpuToUpdate.getName();
-		
+
 		//Company id
 		int nCompany = askNewCompanyId(cpuToUpdate.getManufacturerId());
-		
+
 		//Introduced date
 		Date nIntroDate = askNewIntroducedDate(cpuToUpdate.getDateIntroduced());
 		//Discontinued date
 		Date nDisconDate = askNewDiscontinuedDate(cpuToUpdate.getDateDiscontinued());
-		
+
 		computerDao.update(new Computer(cpuToUpdate.getId(), nName, nCompany, nIntroDate, nDisconDate));
 		System.out.println(computerDao.findById(cpuToUpdate.getId()));
 	}
-	
+
 	public static void deleteComputer() {
 		System.out.println("Search which computer you want to delete\n");
 		Computer cpuToDelete = searchComputerDetails();
-		
+
 		printDeleteValidation();
 		int choice2 = askChoice(2);
-		
+
 		if(choice2 == 1)
 			computerDao.delete(cpuToDelete);
 	}
-	
+
 	public static Computer searchComputerDetails() {
 		printComputerQueryMenu();
 		int choice2 = askChoice(2);
-		
+
 		switch(choice2) {
 		case 1://find by Id
 			System.out.print("ENTER THE ID : ");
 			try {
 				int id = Integer.valueOf(scanner.nextLine());
-				
+
 				Computer cpu = computerDao.findById(id);
 				System.out.println(cpu);
-				
+
 				return cpu;
 			}catch(NumberFormatException e) {
 				System.out.println("INVALID ID (integer only)");
@@ -180,18 +181,18 @@ public class Launcher {
 		case 2://find by Name
 			System.out.print("ENTER THE NAME : ");
 			String name = scanner.nextLine();
-			
+
 			Computer cpu = computerDao.findByName(name);
 			System.out.println(cpu);
-			
+
 			return cpu;
 		default:
 			//
 		}
-		
+
 		return null;
 	}
-	
+
 	////////PRINTS////////
 	public static void printMenu() {
 		System.out.println("_____MAIN MENU_____\n\n"
@@ -204,7 +205,7 @@ public class Launcher {
 				+ "[0] EXIT\n"
 				+ "_______________________\n\n");
 	}
-	
+
 	public static void printComputerQueryMenu() {
 		System.out.println("_____COMPUTER DETAILS MENU_____\n\n"
 				+ "[1] Find by Id\n"
@@ -212,172 +213,241 @@ public class Launcher {
 				+ "[0] EXIT\n"
 				+ "_______________________\n\n");
 	}
-	
+
 	public static void printDeleteValidation() {
 		System.out.println("_____ARE YOU SUR ?_____\n\n"
 				+ "[1] YES, I'M SUR\n"
 				+ "[2] CANCEL\n"
 				+ "_______________________\n\n");
 	}
-	
+
+
+
 	public static void printAllComputer() {
-		for(Computer cpu : computerDao.findAll()) {
-			System.out.println(cpu);
-		}
+		List<Computer> allComputers = computerDao.findAll();
+		PageManager<Computer> pageManager = new PageManager<>(allComputers);
+		String choice;
+		
+		do {
+			pageManager.printCurrentPage();
+			pageManager.printPageActions();
+			choice = askPageAction();
+			
+			switch(choice) {
+			case "NEXT":
+				pageManager.next();
+				break;
+			case "PREV":
+				pageManager.previous();
+				break;
+			default:
+				//
+			}
+
+		}while(!choice.equals("STOP"));
 	}
-	
+
 	public static void printAllCompanies() {
-		for(Company company : companyDao.findAll()) {
-			System.out.println(company);
-		}
+		List<Company> allCompanies = companyDao.findAll();
+		PageManager<Company> pageManager = new PageManager<>(allCompanies);
+		String choice;
+		
+		do {
+			pageManager.printCurrentPage();
+			pageManager.printPageActions();
+			choice = askPageAction();
+			
+			switch(choice) {
+			case "NEXT":
+				pageManager.next();
+				break;
+			case "PREV":
+				pageManager.previous();
+				break;
+			default:
+				//
+			}
+
+		}while(!choice.equals("STOP"));
 	}
-	
+
 	////////CHOICE////////
-	
+
 	public static int askChoice(int upper) {
 		System.out.print("CHOICE : ");
 		String choice = scanner.nextLine();
-		
+
 		while(!validChoice(choice, upper)) {
 			System.out.print("CHOICE : ");
 			choice = scanner.nextLine();
 		}
-		
+
 		return Integer.valueOf(choice);
 	}
-	
+
+	public static String askPageAction() {
+		System.out.print("ACTION : ");
+		String choice = scanner.nextLine();
+
+		while(!validPageAction(choice)) {
+			System.out.print("ACTION : ");
+			choice = scanner.nextLine();
+		}
+
+		if(choice.isEmpty() || choice.toUpperCase().equals("NEXT"))
+			return "NEXT";
+		else if(choice.toUpperCase().equals("PREV") || choice.toUpperCase().equals("PREVIOUS"))
+			return "PREV";
+		else if(choice.toUpperCase().equals("STOP") || choice.toUpperCase().equals("0"))
+			return "STOP";
+
+
+		return null;
+	}
+
 	public static int askCompanyId() {
 		String stCompany;
 		do {
 			System.out.print("ENTER THE MANUFACTURER ID (TYPE [LIST] TO PRINT THE LIST OF MANUFACTURER) : ");
 			stCompany = scanner.nextLine();
-			 
+
 			//Print the list of all companies in the DB
 			if(stCompany.toUpperCase().equals("LIST")) {
 				printAllCompanies();
 				System.out.println("ENTER THE MANUFACTURER ID : ");
 				stCompany = scanner.nextLine();
 			}
-			
+
 		}while(!validChoice(stCompany, 43)); 
-		//TO DO gerer le nombre de company limite dynamiquement
-		
+		//TODO gerer le nombre de company limite dynamiquement
+
 		return Integer.valueOf(stCompany);
 	}
-	
+
 	public static int askNewCompanyId(int currentId) {
 		String stCompany;
 		do {
 			System.out.print("ENTER THE NEW MANUFACTURER ID (TYPE [LIST] TO PRINT THE LIST OF MANUFACTURER | EMPTY TO KEEP THE SAME ["+ currentId +"]) : ");
 			stCompany = scanner.nextLine();
-			 
+
 			if(stCompany.isEmpty())
 				return currentId;
-			
+
 			//Print the list of all companies in the DB
 			if(stCompany.toUpperCase().equals("LIST")) {
 				printAllCompanies();
 				System.out.println("ENTER THE MANUFACTURER ID : ");
 				stCompany = scanner.nextLine();
 			}
-			
+
 		}while(!validChoice(stCompany, 43)); 
-		//TO DO gerer le nombre de company limite dynamiquement
-		
+		//TODO gerer le nombre de company limite dynamiquement
+
 		return Integer.valueOf(stCompany);
 	}
-	
+
 	public static Date askIntroducedDate() {
 		String stDate;
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
-		
+
 		System.out.print("WHEN WAS THE COMPUTER INTRODUCED ? (FORMAT dd mm yyyy | LET EMPTY IF YOU DO NOT KNOW) : ");
 		stDate = scanner.nextLine();
-		
+
 		if(stDate.isEmpty())
 			return null;
-		
+
 		try {
 			return new Date(dateFormat.parse(stDate).getTime());
-			
+
 		}catch(ParseException e) {
 			System.out.println("INVALID DATE");
 			return null;
 		}
 	}
-	
+
 	public static Date askNewIntroducedDate(Date currentDate) {
 		String stDate;
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
-		
+
 		System.out.print("WHEN WAS THE COMPUTER INTRODUCED ? (FORMAT dd mm yyyy | EMPTY TO KEEP THE SAME [" + currentDate + "]) : ");
 		stDate = scanner.nextLine();
-		
+
 		if(stDate.isEmpty())
 			return currentDate;
-		
+
 		try {
 			return new Date(dateFormat.parse(stDate).getTime());
-			
+
 		}catch(ParseException e) {
 			System.out.println("INVALID DATE");
 			return null;
 		}
 	}
-	
-	//TO DO verifier que la date est bien posterieur a celle d'introduction
+
+	//TODO verifier que la date est bien posterieur a celle d'introduction
 	public static Date askDiscontinuedDate() {
 		String stDate;
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
-		
+
 		System.out.print("WHEN WAS THE COMPUTER DISCONTINUED ? (FORMAT dd mm yyyy | LET EMPTY IF YOU DO NOT KNOW) : ");
 		stDate = scanner.nextLine();
-		
+
 		if(stDate.isEmpty())
 			return null;
-		
+
 		try {
 			return new Date(dateFormat.parse(stDate).getTime());
-			
+
 		}catch(ParseException e) {
 			System.out.println("INVALID DATE");
 			return null;
 		}
 	}
-	
+
 	public static Date askNewDiscontinuedDate(Date currentDate) {
 		String stDate;
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
-		
+
 		System.out.print("WHEN WAS THE COMPUTER DISCONTINUED ? (FORMAT dd mm yyyy | EMPTY TO KEEP THE SAME [" + currentDate + "]) : ");
 		stDate = scanner.nextLine();
-		
+
 		if(stDate.isEmpty())
 			return currentDate;
-		
+
 		try {
 			return new Date(dateFormat.parse(stDate).getTime());
-			
+
 		}catch(ParseException e) {
 			System.out.println("INVALID DATE");
 			return null;
 		}
 	}
-	
+
 	public static boolean validChoice(String choice, int upper) {
 		int iChoice;
-		
+
 		try{
 			iChoice = Integer.valueOf(choice);
 		}catch (Exception e){
 			System.out.println("INVALID CHOICE (only integer)");
 			return false;
 		}
-		
+
 		if(iChoice >= 0 && iChoice <= upper)
 			return true;
 		else {
 			System.out.println("INVALID CHOICE (out of limit)");
+			return false;
+		}
+	}
+
+	public static boolean validPageAction(String choice) {
+		if(choice.isEmpty() || choice.toUpperCase().equals("NEXT") ||
+				choice.toUpperCase().equals("PREV") || choice.toUpperCase().equals("PREVIOUS") ||
+				choice.toUpperCase().equals("STOP") || choice.toUpperCase().equals("0"))
+			return true;
+		else {
+			System.out.println("INVALID ACTION");
 			return false;
 		}
 	}
