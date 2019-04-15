9d61d61962ff3118430c0d29db93d07651ca1821
Remi DE WISPELAERE <rdewispelaere@excilys?com>
2019-04-15 14:11:36 +0200

    Sprint 4 - Clean warnings

--- src/main/java/service/ComputerService.java
+++ src/main/java/service/ComputerService.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
--- src/main/java/servlet/DeleteComputer.java
+++ src/main/java/servlet/DeleteComputer.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import main.java.dao.ComputerDao;
